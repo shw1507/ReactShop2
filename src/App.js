07bd8cab84c8e1f86b45773ff22897b209706f81
신현우 <qnfskqkddlek@naver.com>
2024-04-02 20:01:00 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="gnb">
+    <img className="layer" alt="Layer" src="layer_1.png" />
+    <div className="navbar">
+      <div className="text-wrapper">Company</div>
+      <div className="text-wrapper">Technology</div>
+      <div className="text-wrapper">Partners</div>
+      <div className="text-wrapper">News</div>
+      <div className="text-wrapper">Contact</div>
     </div>
+  </div>
   );
 }
 
